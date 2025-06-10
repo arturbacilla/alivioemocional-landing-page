6169c9a9cd4f7191ad5b9f0f7b44eb8704b29eaa
arturbacilla <artur.bacilla1@gmail.com>
2025-06-10 17:13:16 -0300

    feat: :sparkles: Added header title and women picture

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,38 @@ import type React from "react";
 
 const App: React.FC = () => {
 	return (
-		<div id="bg-images">
-			<div id="hand-box" className="fixed top-0 left-0 h-[65vh] w-full md:h-screen md:w-[70vw]">
-				<img className="min-h-full min-w-full object-cover opacity-75" src="/hand.png" alt="drowning hand" />
+		<main>
+			<div id="bg-images">
+				<div id="hand-box" className="relative top-0 left-0 h-[68vh] w-full md:h-screen md:w-[70vw]">
+					<img
+						className="absolute z-0 min-h-full min-w-full object-cover opacity-75"
+						src="/hand.png"
+						alt="drowning hand"
+					/>
+					<div id="woman-frame" className="absolute top-8 right-0 z-20 h-[60%] w-[44%] md:h-[70%] md:w-[45%]">
+						<img
+							className="min-h-full rounded-b-[6rem] rounded-tl-[6rem] border-[1rem]! border-primary! border-solid! object-cover object-[65%-0] md:rounded-b-[20rem] md:rounded-tl-[15rem] md:border-[1.5rem]!"
+							src="/woman.jpeg"
+							alt="wondering woman"
+						/>
+					</div>
+				</div>
+				<div
+					id="balls-box"
+					className="fixed right-[-5rem] bottom-[-8rem] size-[15rem] md:right-[-5rem] md:bottom-[-15rem] md:size-[30rem]"
+				>
+					<img className="min-h-full min-w-full object-fill" src="/balls.svg" alt="decor-balls" />
+				</div>
 			</div>
-			<div
-				id="balls-box"
-				className="fixed right-[-5rem] bottom-[-8rem] size-[15rem] md:right-[-5rem] md:bottom-[-15rem] md:size-[30rem]"
-			>
-				<img className="min-h-full min-w-full object-fill" src="/balls.svg" alt="decor-balls" />
-			</div>
-		</div>
+			<header className="fixed top-[4rem] z-100 flex w-[70%] flex-col rounded-r-[10rem] bg-primary p-[0.8rem]! font-garet! shadow-2xl md:w-[50%] md:items-center md:rounded-r-[20rem] md:p-[2rem]!">
+				<div className="fixed top-[1rem] left-[1rem] flex flex-row items-center gap-2">
+					<img className="size-[2rem]" src="/book.svg" alt="book icon" />
+					<text>EBOOK</text>
+				</div>
+				<text className="font-bold text-[1.3rem] md:text-[3.5rem]">Alívio Emocional</text>
+				<text className="font-bold text-[2.7rem] leading-none md:text-[7rem]">IMEDIATO</text>
+			</header>
+		</main>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'brand-primary': '#4A90E2', // Um azul calmante
-          'brand-secondary': '#50E3C2', // Um verde esperançoso
-          'brand-text': '#333333',
-          'brand-light-bg': '#F8F9FA',
-        },
-        fontFamily: {
-          sans: ['"Open Sans"', 'sans-serif'], // Uma fonte limpa e legível
-          serif: ['"Merriweather"', 'serif'], // Para citações ou ênfases
-        }
-      },
-    },
-    plugins: [],
-  }
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"brand-primary": "#4A90E2", // Um azul calmante
+				"brand-secondary": "#50E3C2", // Um verde esperançoso
+				"brand-text": "#333333",
+				"brand-light-bg": "#F8F9FA",
+			},
+			fontFamily: {
+				sans: ['"Open Sans"', "sans-serif"], // Uma fonte limpa e legível
+				serif: ['"Merriweather"', "serif"], // Para citações ou ênfases
+				garet: ['"Garet"', "sans-serif"],
+				"fredoka-one": ['"Fredoka One"', "sans-serif"],
+				"gochi-hand": ['"Gochi Hand"', "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};

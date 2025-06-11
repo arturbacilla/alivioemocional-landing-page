fce7b5ac263e6e763ef01bb50ef55ec606f99c44
arturbacilla <artur.bacilla1@gmail.com>
2025-06-11 17:34:34 -0300

    feat: :sparkles: Few changes (added main text)
    - Added main text
    - Removed unused shadcn components
    - Added data to fill cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import type React from "react";
 
 const App: React.FC = () => {
+	const list = [
+		"Ferramentas que funcionam na hora da crise.",
+		"Técnicas de respiração, presença e autocompaixão.",
+		"Apoio para lidar com ansiedade, tristeza e dor profunda.",
+		"Reconexão com o sentido da vida, no seu tempo",
+	];
+
 	return (
-		<main>
-			<div id="bg-images">
-				<div id="hand-box" className="relative top-0 left-0 h-[68vh] w-full lg:h-screen lg:w-[70vw]">
+		<main className="select-none">
+			<section id="bg-images" className="relative ">
+				<div id="hand-box" className="absolute top-0 left-0 h-[68vh] w-full lg:h-screen lg:w-[70vw]">
 					<img
 						className="absolute z-0 min-h-full min-w-full object-cover opacity-75"
 						src="/hand.png"
@@ -24,15 +31,28 @@ const App: React.FC = () => {
 				>
 					<img className="min-h-full min-w-full object-fill" src="/balls.svg" alt="decor-balls" />
 				</div>
-			</div>
-			<header className="fixed top-[4rem] z-100 flex w-[70%] flex-col rounded-r-[10rem] bg-primary p-[0.8rem]! font-garet! shadow-2xl md:items-center lg:w-[50%] lg:rounded-r-[20rem] lg:p-[2rem]!">
+			</section>
+			<header className="absolute top-[4rem] z-100 flex w-[70%] flex-col rounded-r-[10rem] bg-primary p-[0.8rem]! font-garet! shadow-2xl md:items-center lg:w-[50%] lg:rounded-r-[20rem] lg:p-[2rem]!">
 				<div className="fixed top-[1rem] left-[1rem] flex flex-row items-center gap-2">
 					<img className="size-[2rem]" src="/book.svg" alt="book icon" />
-					<text>EBOOK</text>
+					<span>EBOOK</span>
 				</div>
-				<text className="font-bold text-[1.3rem] md:text-[2.2rem] lg:text-[3.5rem]">Alívio Emocional</text>
-				<text className="font-bold text-[2.7rem] leading-none md:text-[5rem] lg:text-[7rem]">IMEDIATO</text>
+				<span className="font-bold text-[1.3rem] md:text-[2.2rem] lg:text-[3.5rem]">Alívio Emocional</span>
+				<span className="font-bold text-[2.7rem] leading-none md:text-[5rem] lg:text-[7rem]">IMEDIATO</span>
 			</header>
+			<section
+				id="main-text"
+				className="absolute top-[11rem] left-[1rem] flex h-[40%] w-[12rem] flex-col gap-[0.8rem] font-bold font-garet! lg:top-[22rem] lg:left-[2rem] lg:w-[35%] "
+			>
+				<span className="text-secondary lg:text-[2.4rem]">Um refúgio seguro</span>
+				<span className="text-[0.8rem] text-black lg:text-[1.8rem]">O que você vai encontrar aqui:</span>
+				{list.map((item: string, id: number) => (
+					<div className="flex flex-row gap-1 text-[0.9rem] lg:text-[1.4rem]" key={`item-${id + 1}`}>
+						<div>🌿</div>
+						<span>{item}</span>
+					</div>
+				))}
+			</section>
 		</main>
 	);
 };

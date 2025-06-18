a9544cddea27e4faf1738b1ef2669dab7dd87b61
arturbacilla <artur.bacilla1@gmail.com>
2025-06-18 15:19:51 -0300

    chore: :wrench: Added PORT to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import tailwindcss from "@tailwindcss/vite";
 import biomePlugin from "vite-plugin-biome";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss(), biomePlugin(), ViteImageOptimizer()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }: ConfigEnv) => {
+	const cwd = process.cwd();
+	const env = { ...loadEnv(mode, cwd, "VITE_") };
+
+	return {
+		plugins: [react(), tailwindcss(), biomePlugin(), ViteImageOptimizer()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
+		preview: {
+			host: true,
+			port: Number(env.VITE_PORT ?? 5173),
+		},
+		server: {
+			host: true,
+			port: Number(env.VITE_PORT ?? 5173),
+		},
+	};
 });
